e8e4274a42965bbf2ef16b2f7e27975575aa79f4
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2017-10-04 15:47:25 -0400

    Call onload on beforMount

--- src/index.js
+++ src/index.js
@@ -3,26 +3,28 @@ import Vue from 'vue';
 import urls from './urls.js';
 import axios from 'axios';
 
-(function(){
-let app = new Vue({
-    el: "#app",
-    data: {
-        json: ""
-    },
-    methods: {
-        getWordPressPosts : function(url){
-            return axios.get(url);
+(function () {
+    let app = new Vue({
+        el: "#app",
+        data: {
+            json: ""
         },
-        onload: function(url){
-            let wordPressPromise = this.getWordPressPosts(url);
-            wordPressPromise.then((success)=>{
-                // console.log(success)
-                this.json = success.data.posts[0];
-            }).catch((error)=>{
-                console.log(error);
-            });
+        methods: {
+            getWordPressPosts: function (url) {
+                return axios.get(url);
+            },
+            onload: function (url) {
+                let wordPressPromise = this.getWordPressPosts(url);
+                wordPressPromise.then((success) => {
+                    // console.log(success)
+                    this.json = success.data.posts[0];
+                }).catch((error) => {
+                    console.log(error);
+                });
+            }
+        },
+        beforeMount: function () {
+            this.onload(urls.apiCall);
         }
-    }
-});
-app.onload(urls.apiCall);
+    });
 })();
